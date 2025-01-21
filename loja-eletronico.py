560de27e9dfdcf83c4d2bbb16ff9a8ccddb82a44
Fabiano Novais <fabiano.novais.99@gmail.com>
2025-01-21 13:40:20 -0300

    Integrando a funcionalidade de excluir e adicionar novos produtos ao estoque, a funcionalidade de excluir conta com checagem se o item realmente não pertence mais ao dicionário

--- loja-eletronico.py
+++ loja-eletronico.py
@@ -5,9 +5,9 @@ estoque = { "1":{ "SKU":"TE001", "Descrição":  "Teclado Mecânico Preto Switch
             "3":{ "SKU":"TE003", "Descrição":  "Teclado Mecânico Branco Switch Marrom RGB", "Preço (R$)": 155.00, "Quantidade": 48},
             "4":{ "SKU":"TE004", "Descrição":  "Teclado Mecânico Preto Switch Marrom RGB", "Preço (R$)": 140.00, "Quantidade": 9},
             "5":{ "SKU":"MOS01", "Descrição":  "Mouse RGB Gamer 12 Botões Preto 20000 DPI", "Preço (R$)": 90.00, "Quantidade": 112},
-            "6":{ "SKU":"MOS02", "Descrição":  "Mouse RGB Gamer 12 Botões Preto 20000 DPI", "Preço (R$)": 90.00, "Quantidade": 112}
+            "6":{ "SKU":"MOS02", "Descrição":  "Mouse RGB Gamer 12 Botões Branco 20000 DPI", "Preço (R$)": 110.00, "Quantidade": 25}
 }
-
+contador_de_ids = len(estoque) + 1
 
 def limpar_tela():
     os.system ("cls" if os.name == "nt" else "clear")
@@ -21,7 +21,37 @@ def acessar_estoque():
         print(f"ID: {id}")
         for topico, valor_topico in produto.items():
             print (f"{topico}: {valor_topico}")
-    
+
+def adicionar_produto():
+    global contador_de_ids
+    print ("Vamos adicionar um novo produto ao estoque, para isso ensira algumas informações:")
+    sku_novo = str(input("Ensira o código inteiro no produto (SKU): "))
+    descricao_novo = str(input("Ensira a Descrição de seu produto: "))
+    preco_novo = float(input("Ensira o preço padrão do Produto (R$): "))
+    quantidade_novo = int(input("Ensira a quantidade que estará disponível em estoque: "))
+
+    produto_novo = {"SKU": sku_novo, "Descrição": descricao_novo, "Preço (R$)": preco_novo, "Quantidade": quantidade_novo}
+
+    estoque [ str(contador_de_ids)] = produto_novo
+    if estoque [ str(contador_de_ids)] == produto_novo:
+        print ("Produto adicionado com sucesso")
+        contador_de_ids += 1
+    else:
+        print ("Houve um erro, tente novamente mais tarde")
+    pause()
+
+def excluir_produto():
+    produto_excluir = str(input("Digite a ID do produto que deseja excluir: "))
+    del estoque[produto_excluir]
+    produto_check = False
+    for chave in estoque:
+        if chave == produto_excluir:
+            produto_check = True
+            break
+    if produto_check == True:
+        print ("Houve um erro ao excluir o produto. Tente novamente mais tarde!")
+    else:
+        print (f"ID: {produto_excluir} - excluído com sucesso")
 
 def menu_selecionado(opcao_selecionada):
     if opcao_selecionada == 1:
@@ -35,14 +65,25 @@ def menu_selecionado(opcao_selecionada):
         print("-------------------------------------")
         print("--------- Adicionar Produto ---------")
         print("-------------------------------------")
+        acessar_estoque()
+        adicionar_produto()
     elif opcao_selecionada == 3:
         print("-------------------------------------")
         print("---- Atualizar Produto Existente ----")
         print("-------------------------------------")
+        acessar_estoque()
     elif opcao_selecionada == 4:
         print("-------------------------------------")
         print("---------- Excluir Produto ----------")
         print("-------------------------------------")
+        acessar_estoque()
+        excluir_produto()
+        resposta = input ("Visualizar estoque atualizado? [s/n] ")
+        if resposta == 's':
+            acessar_estoque()
+        pause()
+        limpar_tela()
+
     elif opcao_selecionada == 0:
         print("-------------------------------------")
         print("-------- Operação Encerrada ---------")
